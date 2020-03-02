f0748106a9381aab6bc5c0b75dee87ba0d7b45d5
james stewart <jamesstewart@rrcs-24-123-231-214.central.biz.rr.com>
2020-03-02 13:15:18 -0500

    add more inital stuff

--- file_to_prefetch.py
+++ file_to_prefetch.py
@@ -3,12 +3,17 @@
 #  - https://github.com/jgstew/tools/blob/master/Python/url_to_prefetch.py
 #  - https://bigfix.me/relevance/details/3022868
 
+import os
 from hashlib import sha1, sha256
 
 
-def main():
-  print( "TODO: all the things" )
+def file_to_prefetch(file_path):
+    return "TODO: all the things " + file_path
+
+
+def main(file_path="LICENSE"):
+    print(file_to_prefetch(file_path))
 
 # if called directly, then run this example:
 if __name__ == '__main__':
-  main()
+    main()
