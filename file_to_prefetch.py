76282773e307c7fd76665691790505191e7fd450
jgstew <james@jgstew.com>
2020-03-02 13:24:59 -0500

    add docstrings

--- file_to_prefetch.py
+++ file_to_prefetch.py
@@ -1,17 +1,21 @@
-
+#!/usr/bin/env python
+"""This script takes a file path and optionally a URL and generates
+ a bigfix prefetch statement."""
 # Related:
 #  - https://github.com/jgstew/tools/blob/master/Python/url_to_prefetch.py
 #  - https://bigfix.me/relevance/details/3022868
 
-import os
-from hashlib import sha1, sha256
+#import os
+#from hashlib import sha1, sha256
 
 
-def file_to_prefetch(file_path):
-    return "TODO: all the things " + file_path
+def file_to_prefetch(file_path, url="http://unknown"):
+    """Return the bigfix prefetch generated from the provided file"""
+    return "TODO: all the things. file:" + file_path + " url:" + url
 
 
 def main(file_path="LICENSE"):
+    """Only called if this script is run directly"""
     print(file_to_prefetch(file_path))
 
 # if called directly, then run this example:
