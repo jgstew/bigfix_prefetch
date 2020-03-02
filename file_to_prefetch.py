181b1abf924f4fbf1d3f589e415ea4730766a2a1
jgstew <james@jgstew.com>
2020-03-02 14:22:11 -0500

    main parts of prefetch generating are working

--- file_to_prefetch.py
+++ file_to_prefetch.py
@@ -14,17 +14,29 @@ from hashlib import sha1, sha256
 def file_to_prefetch(file_path, url="http://unknown"):
     """Return the bigfix prefetch generated from the provided file"""
     hashes = sha1(), sha256()
-    chunksize = max(4*1024, max(h.block_size for h in hashes))
+    chunk_size = max(4*1024, max(h.block_size for h in hashes))
+    file_size = 0
 
     if os.path.isfile(file_path) and os.access(file_path, os.R_OK):
-        print("file exists! " + file_path)
+        print("Debug_Info:: file exists! " + file_path)
     else:
         return "Error: file does not exist or is not readable! " + file_path
+    
+    with open(file_path, 'rb') as file_object:
+        while True:
+            chunk = file_object.read(chunk_size)
+            if not chunk:
+                break
+            # NOTE: This is probably not needed, could read directly from filesystem
+            file_size += len(chunk)
+            for h in hashes:
+                h.update(chunk)
 
-    return "PLACEHOLDER:: file:" + file_path + " url:" + url + \
-                " chunksize:" + str(chunksize)
-    # return ( "prefetch %s sha1:%s size:%d %s sha256:%s" % \
-    #       (filename, hashes[0].hexdigest(), size, url, hashes[1].hexdigest()) )
+
+    print("Debug_Info:: file:" + file_path + " url:" + url + \
+                " chunksize:" + str(chunk_size))
+    return ( "prefetch %s sha1:%s size:%d %s sha256:%s" % \
+                (file_path, hashes[0].hexdigest(), file_size, url, hashes[1].hexdigest()) )
 
 
 def main(file_path="LICENSE"):
