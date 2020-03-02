55e3f39199cb3ad2e9b9ee2bd7ac992d43fb9e46
james stewart <jamesstewart@rrcs-24-123-231-214.central.biz.rr.com>
2020-03-02 13:08:45 -0500

    initial commit

---
+++ file_to_prefetch.py
@@ -0,0 +1,14 @@
+
+# Related:
+#  - https://github.com/jgstew/tools/blob/master/Python/url_to_prefetch.py
+#  - https://bigfix.me/relevance/details/3022868
+
+from hashlib import sha1, sha256
+
+
+def main():
+  print( "TODO: all the things" )
+
+# if called directly, then run this example:
+if __name__ == '__main__':
+  main()
