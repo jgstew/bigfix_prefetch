c7b07d76df52a54892aad66892c59a41a6e9f12b
jgstew <james@jgstew.com>
2020-03-15 14:46:23 -0400

    pylint fixes

--- parse_prefech.py
+++ parse_prefech.py
@@ -3,9 +3,10 @@
 
 from __future__ import absolute_import
 
-import re
+#import re
 
 def parse_prefetch(prefetch):
+    """parse bigfix prefetch using regex"""
     parsed_prefetch = {}
     parsed_prefetch['raw_prefech'] = prefetch
 
@@ -20,10 +21,13 @@ def parse_prefetch(prefetch):
         print("- prefetch block:")
     return parsed_prefetch['raw_prefech']
 
-def main(prefetch="add prefetch item name=LGPO.zip sha1=0c74dac83aed569607aaa6df152206c709eef769 size=815660 url=https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip sha256=6ffb6416366652993c992280e29faea3507b5b5aa661c33ba1af31f48acea9c4"):
+def main(prefetch="add prefetch item name=LGPO.zip sha1=0c74dac83aed569607aaa6df152206c709eef769 \
+size=815660 url=https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip \
+sha256=6ffb6416366652993c992280e29faea3507b5b5aa661c33ba1af31f48acea9c4"):
     """Only called if this script is run directly"""
     print(parse_prefetch(prefetch))
-    print(parse_prefetch("prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"))
+    print(parse_prefetch("prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 \
+size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"))
 
 # if called directly, then run this example:
 if __name__ == '__main__':
