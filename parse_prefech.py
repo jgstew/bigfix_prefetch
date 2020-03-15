b008fc2e42504abf71420f55d0430eef646d5c45
jgstew <james@jgstew.com>
2020-03-15 16:40:56 -0400

    pylint fixes, added prefetch type

--- parse_prefech.py
+++ parse_prefech.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 """This script takes a bigfix prefetch and parses it into a hash dictionary
 
-This is the opposite of https://github.com/jgstew/generate_bes_from_template/blob/master/action_prefetch_from_template.py
+This is the opposite of:
+- https://github.com/jgstew/generate_bes_from_template/blob/master/action_prefetch_from_template.py
 """
 
 from __future__ import absolute_import
@@ -20,6 +21,7 @@ def parse_prefetch(prefetch):
         # get sha1:  / sha1:(\w{40}) /
         # get sha256:  / sha256:(\w{64})/
         # get url:  / (\S+://\S+)/
+        parsed_prefetch['prefetch_type'] = "statement"
         parsed_prefetch['file_name'] = re.search(r'prefetch (\S+) ', prefetch).group(1)
         parsed_prefetch['file_size'] = re.search(r' size:(\d+) ', prefetch).group(1)
         parsed_prefetch['file_sha1'] = re.search(r' sha1:(\w{40}) ', prefetch).group(1)
@@ -36,6 +38,7 @@ def parse_prefetch(prefetch):
         # get sha1:  / sha1=(\w{40})/
         # get sha256:  / sha256=(\w{64})/
         # get url:  / url=(\S+://\S+)/
+        parsed_prefetch['prefetch_type'] = "block"
         parsed_prefetch['file_name'] = re.search(r' name=(\S+)', prefetch).group(1)
         parsed_prefetch['file_size'] = re.search(r' size=(\d+)', prefetch).group(1)
         parsed_prefetch['file_sha1'] = re.search(r' sha1=(\w{40})', prefetch).group(1)
@@ -50,8 +53,9 @@ def parse_prefetch(prefetch):
 
 def main():
     """Only called if this script is run directly"""
-    print(parse_prefetch("add prefetch item name=LGPO.zip sha1=0c74dac83aed569607aaa6df152206c709eef769 \
-size=815660 url=https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip \
+    print(parse_prefetch("add prefetch item name=LGPO.zip \
+sha1=0c74dac83aed569607aaa6df152206c709eef769 size=815660 \
+url=https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip \
 sha256=6ffb6416366652993c992280e29faea3507b5b5aa661c33ba1af31f48acea9c4"))
     print(parse_prefetch("prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 \
 size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"))
