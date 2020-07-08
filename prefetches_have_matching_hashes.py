fbd1d4baff0d9b9f71ab0e8dae6be33a607361c9
jgstew <james@jgstew.com>
2020-07-08 16:39:45 -0400

    added sha256 check

--- prefetches_have_matching_hashes.py
+++ prefetches_have_matching_hashes.py
@@ -16,14 +16,24 @@ def prefetches_have_matching_hashes(prefetch_one, prefetch_two):
     # ensure both prefetches have the required details
     if not ( ( parsed_prefetch_one.keys() >= {"file_size", "file_sha1"} ) and ( parsed_prefetch_two.keys() >= {"file_size", "file_sha1"} ) ):
         print("invalid prefetch")
+        if ( ( parsed_prefetch_one.keys() >= {"file_size", "file_md5"} ) and ( parsed_prefetch_two.keys() >= {"file_size", "file_md5"} ) ):
+            print("Both Have MD5 - Is this for comparison?")
         return False
 
     try:
         # file_size could be an int or a string, force convertion to int for comparison. 
         if int(parsed_prefetch_one['file_size']) == int(parsed_prefetch_two['file_size']):
             if parsed_prefetch_one['file_sha1'] == parsed_prefetch_two['file_sha1']:
+                if ( ( parsed_prefetch_one.keys() >= {"file_sha256"} ) and ( parsed_prefetch_two.keys() >= {"file_sha256"} ) ):
                 # NOTE: need to also check sha256 if present
-                return True
+                    if parsed_prefetch_one['file_sha256'] == parsed_prefetch_two['file_sha256']:
+                        return True
+                    else:
+                        print("ERROR: file_sha256 doesn't match")
+                        return False
+                else:
+                    print("Warning: file_sha256 is missing but size and sha1 match")
+                    return True
             else:
                 print("ERROR: file_sha1 doesn't match")
         else:
@@ -32,6 +42,7 @@ def prefetches_have_matching_hashes(prefetch_one, prefetch_two):
         print("ERROR: Invalid file_size")
         return False
 
+    # catch all:
     return False
 
 
