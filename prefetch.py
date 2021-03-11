1f20facd812335648e23c52cb22902733c6616b0
James Stewart <james.stewart@hcl.com>
2021-03-11 11:41:43 -0500

    pylint fixes, path fix in flake8.yaml

--- prefetch.py
+++ prefetch.py
@@ -12,6 +12,12 @@ This script accepts a prefetch statement, or prefetch block, or a dictionary wit
 # Related:
 #  - https://github.com/jgstew/tools/blob/master/Python/url_to_prefetch.py
 
+import site
+import os.path
+
+# add path this script is in
+site.addsitedir(os.path.dirname(os.path.abspath(__file__)))
+
 import parse_prefetch
 import url_to_prefetch
 import prefetches_have_matching_hashes
@@ -39,28 +45,30 @@ def prefetch(prefetch_data, save_file=True):
         print(file_path)
 
     # regenerate the prefetch, then compare.
-    test_prefetch = url_to_prefetch.url_to_prefetch( parsed_prefetch['download_url'], True, file_path )
+    test_prefetch = url_to_prefetch.url_to_prefetch(
+        parsed_prefetch['download_url'],
+        True,
+        file_path
+    )
 
     print(test_prefetch)
     print(parsed_prefetch)
 
-    return prefetches_have_matching_hashes.prefetches_have_matching_hashes( parsed_prefetch, test_prefetch )
+    return prefetches_have_matching_hashes.prefetches_have_matching_hashes(
+        parsed_prefetch,
+        test_prefetch
+    )
 
 
 def main():
     """Only called if this script is run directly"""
-    # print(prefetch("add prefetch item name=LGPO.zip sha1=0c74dac83aed569607aaa6df152206c709eef769 \
-    # size=815660 url=https://download.microsoft.com/download/8/5/C/85C25433-A1B0-4FFA-9429-7E023E7DA8D8/LGPO.zip \
-    # sha256=6ffb6416366652993c992280e29faea3507b5b5aa661c33ba1af31f48acea9c4"))
-    # print(prefetch("prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 \
-    # size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"))
     prefetch_dictionary_one = {
-            'file_name': 'unzip.exe',
-            'file_size': '167936',
-            'file_sha1': 'e1652b058195db3f5f754b7ab430652ae04a50b8',
-            'download_url': 'http://software.bigfix.com/download/redist/unzip-5.52.exe'
-            }
-    print( prefetch( prefetch_dictionary_one , False ) )
+        'file_name': 'unzip.exe',
+        'file_size': '167936',
+        'file_sha1': 'e1652b058195db3f5f754b7ab430652ae04a50b8',
+        'download_url': 'http://software.bigfix.com/download/redist/unzip-5.52.exe'
+    }
+    print(prefetch(prefetch_dictionary_one, False))
 
 
 # if called directly, then run this example:
