8e9c975d6f5457010752fa49e9d7b47983992fe6
JGStew <james@jgstew.com>
2024-10-23 11:26:16 -0400

    update example

--- examples/add_sha256_prefetch.py
+++ examples/add_sha256_prefetch.py
@@ -4,24 +4,8 @@ This example is how to update a prefetch to add sha256 hashes to a prefetch.
 This WILL cause the file to be downloaded over the internet in the prefetch, though the file will not be saved to disk by default.
 """
 
-# NOTE: this example requires bigfix_prefetch 1.1.4+
+# NOTE: this example requires bigfix_prefetch 1.1.5+
 import bigfix_prefetch
-
-
-def add_sha256_prefetch(prefetch_to_update, save_file=False):
-    """This function will update a prefetch to add sha256"""
-
-    # get prefetch type: (block or statement)
-    parsed = bigfix_prefetch.prefetch_parse.parse_prefetch(prefetch_to_update)
-
-    # get updated prefetch: (the False means the file will not be saved to disk)
-    updated_prefetch = bigfix_prefetch.prefetch.prefetch(prefetch_to_update, save_file)
-
-    updated_prefetch["prefetch_type"] = parsed["prefetch_type"]
-
-    return bigfix_prefetch.prefetch_from_dictionary.prefetch_from_dictionary(
-        updated_prefetch
-    )
 
 
 def main():
@@ -29,7 +13,7 @@ def main():
     prefetch_to_update = "prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"
 
     print("Updated Prefetch:")
-    print(add_sha256_prefetch(prefetch_to_update))
+    print(bigfix_prefetch.prefetch.add_sha256_prefetch(prefetch_to_update))
 
 
 # if called directly, then run this example:
