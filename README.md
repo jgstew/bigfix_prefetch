6b070e26769b794d916d258f924e2bcf3894ad6c
JGStew <james@jgstew.com>
2024-10-23 10:35:55 -0400

    add example

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # bigfix-prefetch
 Python scripts for working with BigFix prefetches
 
-[Tested](https://github.com/jgstew/bigfix_prefetch/actions/workflows/test_src.yaml) on Mac, Windows, Linux on Python 2 and Python 3
+[Tested](https://github.com/jgstew/bigfix_prefetch/actions/workflows/test_src.yaml) on Mac, Windows, Linux on Python 3
+
+This does theoretically support Python2 but is no longer tested and validated for Python2.
 
 ## Install through pip
 
---
+++ examples/add_sha256_prefetch.py
@@ -0,0 +1,27 @@
+# NOTE: this example requires bigfix_prefetch 1.0.4+
+import bigfix_prefetch
+
+
+def main():
+    """Only called if prefetch is run directly"""
+    prefetch_to_update = "prefetch unzip.exe sha1:e1652b058195db3f5f754b7ab430652ae04a50b8 size:167936 http://software.bigfix.com/download/redist/unzip-5.52.exe"
+
+    # get prefetch type: (block or statement)
+    parsed = bigfix_prefetch.prefetch_parse.parse_prefetch(prefetch_to_update)
+
+    # get updated prefetch:
+    updated_prefetch = bigfix_prefetch.prefetch.prefetch(prefetch_to_update, False)
+
+    updated_prefetch["prefetch_type"] = parsed["prefetch_type"]
+
+    print("Updated Prefetch:")
+    print(
+        bigfix_prefetch.prefetch_from_dictionary.prefetch_from_dictionary(
+            updated_prefetch
+        )
+    )
+
+
+# if called directly, then run this example:
+if __name__ == "__main__":
+    main()
