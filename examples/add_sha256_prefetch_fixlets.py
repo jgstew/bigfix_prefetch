5a28ccb9fbf4569613d0b30bfb34a4c3430735cf
JGStew <james@jgstew.com>
2024-12-06 10:49:02 -0500

    working example

---
+++ examples/add_sha256_prefetch_fixlets.py
@@ -0,0 +1,67 @@
+"""
+This example is how to update a prefetch to add sha256 hashes to a prefetch.
+
+This WILL cause the file to be downloaded over the internet in the prefetch, though the file will not be saved to disk by default.
+"""
+
+import re
+
+import besapi
+
+# NOTE: this example requires bigfix_prefetch 1.1.5+
+import bigfix_prefetch
+
+
+def update_fixlet_prefetch(file_path):
+    """update fixlet files"""
+    print("update_fixlet_prefetch(file_path)")
+
+    file_contents = ""
+
+    # read file contents
+    with open(file_path, "r") as file:
+        # Reading from a file
+        file_contents = file.read()
+        # print(file_contents)
+
+        # extract prefetch statement (or block?)
+        prefetches = re.findall(".*prefetch.*$", file_contents, re.MULTILINE)
+        for prefetch in prefetches:
+            prefetch = prefetch.strip()
+            print(bigfix_prefetch.prefetch_parse.parse_prefetch(prefetch))
+
+            # update prefetch
+            updated_prefetch = bigfix_prefetch.prefetch.add_sha256_prefetch(prefetch)
+
+            file_contents = file_contents.replace(prefetch, updated_prefetch)
+
+            # print(file_contents)
+
+    # write updated file contents back
+    with open(file_path, "w") as file:
+        file.writelines(file_contents)
+
+
+def main():
+    """Only called if this script is run directly"""
+
+    bes_conn = besapi.besapi.get_bes_conn_using_config_file()
+
+    fixlets_to_fix_relevance = """ ("fixlet/custom/" & name of site of it & "/" & id of it as string) of fixlets whose( (fixlet flag of it OR task flag of it) AND exists scripts whose(it as lowercase contains "prefetch" AND it as lowercase does not contain "sha256") of actions of it ) of bes custom sites whose(name of it = "Demo") """
+
+    results = bes_conn.session_relevance_array(fixlets_to_fix_relevance)
+
+    print(results)
+
+    for fixlet in results:
+        # https://developer.bigfix.com/rest-api/api/fixlet.html
+        export_file_path = bes_conn.export_item_by_resource(
+            fixlet, "./examples/fixlets_to_fix"
+        )
+        print(export_file_path)
+        update_fixlet_prefetch(export_file_path)
+
+
+# if called directly, then run this example:
+if __name__ == "__main__":
+    main()
